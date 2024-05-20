c3773b71faf3c7c1490008f9d53af474bd704c06
Guitar <kittipong.sataya@gmail.com>
2024-05-20 14:25:48 +0700

    first submit

---
+++ getElement.js
@@ -0,0 +1,9 @@
+function changeText() {
+    let elementH2 = document.getElementById("Heading");
+    elementH2.innerText = "Hello Java Script InnerHTML";
+}
+
+
+function reloadPage() {
+	window.location.reload();
+}
