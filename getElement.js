5c1ad819a3df3fe0b8b4e997edac10065a0d9b5a
Guitar <kittipong.sataya@gmail.com>
2024-05-20 23:27:15 +0700

    second submit

--- getElement.js
+++ getElement.js
@@ -1,9 +1,15 @@
+//Call function changetext() from html file then change text to "hello javascript" 
 function changeText() {
     let elementH2 = document.getElementById("Heading");
     elementH2.innerText = "Hello Java Script InnerHTML";
+    document.body.style.backgroundColor = "pink";
+
 }
 
-
+//Call function reload for reset button()
 function reloadPage() {
 	window.location.reload();
 }
+
+
+
